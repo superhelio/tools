de6539cd66bd033d39a9e8f7bff2f24c37de6949
Ismo Vuorinen <ismo.vuorinen@superhelio.com>
2016-08-15 12:27:51 +0300

    Remove extra new line before Full changelog link

--- release.sh
+++ release.sh
@@ -131,7 +131,6 @@ fi
 echo "## $NEW_VERSION ($NOW)" > tmpfile
 git --no-pager log --pretty=format:"  - %s" --date=short --no-merges "$BASE_STRING"...HEAD | sed -n '1!G;h;$p' >> tmpfile
 echo "" >> tmpfile
-echo "" >> tmpfile
 echo "[Full changelog]($PROJECT_URL/$VER_COMP_STR/$VER_COMP_TAG)" >> tmpfile
 echo "" >> tmpfile
 echo "" >> tmpfile
